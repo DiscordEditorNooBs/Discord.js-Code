d344b4d738bec242959169b0143ec873369e4a6b
NooBs <45146973+DiscordEditorNooBs@users.noreply.github.com>
2018-12-29 00:39:19 +0900

    Update index.js

--- Embed/index.js
+++ Embed/index.js
@@ -15,7 +15,7 @@ client.on('message', message => {
     const embed = new RichEmbed()
       .setTitle('텍스트 타이틀') //텍스트의 타이틀을 정합니다.
       .setColor("RANDOM") //Embed의 색을 랜덤컬러로 정합니다. 색을 정하고 싶다면 ""를 없애고 디스코드 색깔코드를 삽입하세요.
-      .setDescription('타이틀의 보조 ') //타이틀의 보조 텍스트입니다.
+      .setDescription('타이틀의 보조 텍스트') //타이틀의 보조 텍스트입니다.
       .setThumbnail('Embed에 들어갈 사진의 링크') //Embed에 삽입하고 싶은 사진의 링크를 삽입하세요.
       .addField('텍스트의 제목', '텍스트 아래 보조 텍스트') //타이틀 밑에 텍스트를 넣고 보조 텍스트를 넣습니다.
       .addBlankField() //Embed에 빈공간을 추가시킵니다.
